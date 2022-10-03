b41b2651f388ed5504bfd93f01490043df6bc832
aditya <pokemaniacaditya@gmail.com>
2022-10-03 21:22:19 +0530

    top navigation complete

---
+++ src/components/cart-icon/index.tsx
@@ -0,0 +1,9 @@
+const CartIcon = () => {
+  return (
+    <div className="p-2 items-center">
+      <img src="./shopping-bag.svg" width={24} height="24"/>
+    </div>
+  )
+};
+
+export default CartIcon;
---
+++ src/components/top-navigation/index.tsx
@@ -0,0 +1,39 @@
+import { signOut, useSession } from "next-auth/react";
+import { useRouter } from "next/router";
+import CartIcon from "../cart-icon";
+
+const TopNavigation = () => {
+    const { data: session } = useSession();
+    const router = useRouter();
+
+    const handleSignIn = (e: { preventDefault: () => void; }) => {
+        e.preventDefault();
+        router.push("./signin");
+    }
+
+    const handleSignOut = () => {
+        signOut();
+    }
+
+    return (
+        <div className="flex min-w-full justify-between items-center p-8">
+            <div>
+                <img onClick={()=>router.push("./")} className="cursor-pointer" src="/favicon.ico" />
+            </div>
+
+            <div className="flex items-center">
+                <button className="p-2" onClick={()=>router.push("./shop")}>SHOP</button>
+                <button className="p-2">CONTACT</button>
+                {/* <button className="p-2">SIGN IN</button> */}
+                {session ? (
+                    <button onClick={handleSignOut} className="p-2">SIGN OUT</button>
+                ) : (
+                    <button onClick={handleSignIn} className="p-2">SIGN IN</button>
+                )}
+                <CartIcon />
+            </div>
+        </div>
+    )
+}
+
+export default TopNavigation;
