ecd59c08afb9bfb4d2a9b2df60ed6ce84f86558b
aditya <pokemaniacaditya@gmail.com>
2023-01-25 00:03:32 +0530

    fixed build errors

--- src/pages/[id].tsx
+++ src/pages/[id].tsx
@@ -1,6 +1,6 @@
 import TopNavigation from "@/components/top-navigation";
 import { useCartStore } from "@/store/cartStore";
-import ShopItemType from "@/types/shop-item-type";
+import {ShopItemType} from "@/types/shop-item-type";
 import { trpc } from "@/utils/trpc";
 import Head from "next/head";
 import { useRouter } from "next/router";
@@ -35,7 +35,7 @@ const CollectionPage = () => {
           <div className="mx-auto max-w-2xl py-16 px-4 sm:py-24 sm:px-6 lg:max-w-7xl lg:px-8">
             <div className="grid grid-cols-1 gap-y-10 gap-x-6 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8">
               {collection.data?.map((product: ShopItemType) => (
-                <a key={product.id} className="group cursor-pointer" onClick={() => addItem(product)}>
+                <a key={product.itemId} className="group cursor-pointer" onClick={() => addItem(product)}>
                   <div className="aspect-w-1 aspect-h-1 w-full overflow-hidden rounded-lg bg-gray-200 xl:aspect-w-7 xl:aspect-h-8 border-purple-900 border-2">
                     <img
                       src={product.imageUrl}
